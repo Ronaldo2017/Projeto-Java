c010c259ec86681416ea2a020e861e8133d56565
Ronaldo2017 <Ronaldo2017@users.noreply.github.com>
2022-03-12 08:40:20 -0300

    Metódos padrões

---
+++ DefaultMethods/src/services/UsaInterestService2.java
@@ -0,0 +1,18 @@
+package services;
+
+import java.security.InvalidParameterException;
+
+public class UsaInterestService2 implements InterestService{
+	
+	private double interestRate;
+	
+	public UsaInterestService2(double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	@Override
+	public double getInterestRate() {
+		return interestRate;
+	}
+	
+}
